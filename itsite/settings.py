7bc32fdf236dbbca078d3b29f25db93e55ad1314
Vladimir Baranov <vtgk12ak@gmail.com>
2020-12-27 21:18:06 +0500

    setting3

--- itsite/settings.py
+++ itsite/settings.py
@@ -89,7 +89,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vtgk12ak@gmail.com' # От кого
 EMAIL_HOST_PASSWORD = 'hikq mqnb jboh tlsh'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
